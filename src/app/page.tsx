5c6a810536f5a82f85a60eef9562c2e66c593fbf
im <83586607+exodie@users.noreply.github.com>
2024-03-30 15:12:18 +0400

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import styled from "styled-components";
+
+export default function Home() {
+  return <Main>home</Main>;
+}
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
