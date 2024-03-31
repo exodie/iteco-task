ba7e077cfc670a2bc6f68018b3e935a655581a00
im <83586607+exodie@users.noreply.github.com>
2024-03-31 13:22:16 +0400

    refactor main page & switch to 0.1.2 version
    - Add Virtuoso lib for infinite scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,33 @@
 "use client";
 
+import { Virtuoso } from "react-virtuoso";
 import styled from "styled-components";
 
+import { selectTransportation, useAppSelector } from "@/shared/store";
+
+import { TransportationCart } from "@/widgets/cart";
+
 export default function Home() {
-  return <Main>home</Main>;
+  const transportation = useAppSelector(selectTransportation);
+
+  return (
+    <Main>
+      <h1 style={{ marginLeft: 10 }}>Список перевозок</h1>
+      <Virtuoso
+        style={{ height: "685px" }}
+        totalCount={transportation.length}
+        data={transportation}
+        itemContent={(_, trans) => <TransportationCart {...trans} />}
+      />
+    </Main>
+  );
 }
 
 const Main = styled.main`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+  margin: auto;
+
+  background-color: #f5f5f5;
+
+  max-width: 65vw;
+  min-height: 100vh;
 `;
