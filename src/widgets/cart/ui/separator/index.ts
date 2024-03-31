8a472551a0797422923602e3d120eff39e76ffb9
im <83586607+exodie@users.noreply.github.com>
2024-03-31 13:18:03 +0400

    add separator component for cart

---
+++ src/widgets/cart/ui/separator/index.ts
@@ -0,0 +1,1 @@
+export { CartSeparatorOfItems } from "./ui";
---
+++ src/widgets/cart/ui/separator/ui.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import styled from "styled-components";
+
+export const CartSeparatorOfItems = () => {
+  return <Separator />;
+};
+
+const Separator = styled.div`
+  width: 1px;
+  height: 90px;
+
+  background: #e7e7e7;
+
+  margin: auto 16.25px;
+`;
