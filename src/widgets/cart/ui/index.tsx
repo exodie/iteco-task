4e2585f89cfdea583d4c271820a71189a38e2890
im <83586607+exodie@users.noreply.github.com>
2024-03-31 13:19:35 +0400

    init all cart component to index

--- src/widgets/cart/ui/index.tsx
+++ src/widgets/cart/ui/index.tsx
@@ -1,3 +1,42 @@
-export const TransportationCart = () => {
-  return <div></div>;
+"use client";
+
+import { type FC } from "react";
+import styled from "styled-components";
+
+import { type Transportation } from "@/shared/types";
+
+import { CartRoute } from "./route";
+import { CartSeparatorOfItems } from "./separator";
+import { CartCargo } from "./cargo";
+import { CartPayment } from "./payment";
+
+export const TransportationCart: FC<Transportation> = ({
+  route,
+  cargo,
+  payment,
+}) => {
+  return (
+    <Container>
+      <CartRoute {...route} />
+      <CartSeparatorOfItems />
+      <CartCargo {...cargo} />
+      <CartSeparatorOfItems />
+      <CartPayment {...payment} />
+    </Container>
+  );
 };
+
+const Container = styled.section`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  background: white;
+
+  padding: 32px 25px;
+  margin: 16px 10px;
+
+  border-radius: 16;
+
+  max-width: 1100px;
+`;
